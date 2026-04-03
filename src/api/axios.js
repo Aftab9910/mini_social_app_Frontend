e3d367a4f5fb2e705afd244afb913eb6bd58650a
Aftab9910 <aftab.sde9910@gmail.com>
2026-04-03 23:07:45 +0530

    add vercel.json for routing

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://mini-social-app-backend-j2jq.onrender.com/api"
+  baseURL: "https://mini-social-app-backend-j2jq.onrender.com/api",
 });
