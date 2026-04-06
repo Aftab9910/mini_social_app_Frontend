0655b96f57c0506aff329970b5ff6ddda5c2a1a8
Aftab9910 <aftab.sde9910@gmail.com>
2026-04-06 23:55:14 +0530

    api fixed

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "https://mini-social-app-backend-j2jq.onrender.com/api",
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://mini-social-app-backend-j2jq.onrender.com/api",
 });
